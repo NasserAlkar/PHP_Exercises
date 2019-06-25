6edec916454f6bdf68e1808b5034165724005835
nasser <alkar0abdulnasser@gmail.com>
2019-06-25 11:04:35 +0200

    second commit

---
+++ Day1_Exercsise/exercises5.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>characteristics</title>
+
+</head>
+<body>
+<?php
+ $myHero ='<h1> My Hero </h1>';
+ echo $myHero . '<br>' ;
+ $MyHeroImage = '<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/51158316-fd7e-48ca-b5fe-8542e9dfe357/dc6mse3-55695d6a-285c-413b-a30b-2c245ece6622.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzUxMTU4MzE2LWZkN2UtNDhjYS1iNWZlLTg1NDJlOWRmZTM1N1wvZGM2bXNlMy01NTY5NWQ2YS0yODVjLTQxM2ItYTMwYi0yYzI0NWVjZTY2MjIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.10LCrhT7gQpR2GtIHMLKzH7rRtOd5oRg6YPKEpk9P2A" alt="">' . '<br>' ;
+ echo $MyHeroImage . '<br>' ;
+
+ $heroLastName = '<h1>Akagami</h1>';
+ echo $heroLastName ;
+
+ $heroFirstName = '<h1>Shanks</h1>';
+ echo $heroFirstName;
+
+$characteristics = [
+    'attakPoint'=>'5586',
+    'defensePoint'=>'99447',
+    'Hacky' => ' kings Hacky '
+];
+
+
+
+echo 'attak Point ; ' . $characteristics['attakPoint'] . '<br>' ;
+echo 'defense Point; ' . $characteristics['defensePoint'] . '<br>';
+echo 'Hacky tybe ; ' . $characteristics['Hacky'] . '<br>';
+
+
+?>
+
+<p>Name; 
+<?php echo $heroFirstName . ' ' . $heroLastName ?>
+</p>
+<img src="<?php echo $MyHeroImage ?>" alt="">
+
+</body>
+</html>
--- Day1_Exercsise/index.php
+++ Day1_Exercsise/index.php
@@ -10,9 +10,47 @@
     
 
     <?php
-    echo '<h1> Thisis my '
+    echo '<h1> This is my first exercises </h1>';
     
-    
+    $fruit =[
+        'apple',
+        'Strawberry',
+        'Pineapple',
+        'Lemon'
+    ];
+    echo $fruit[2] . '<br>';
+
+    $clothes =[
+        'T-Shirt' => '20',
+        'Caps' => '10',
+        'Shoes' => '5'
+    ];
+    echo $clothes["Caps"]  . '<br>' ;
+
+    $clothes['Caps']+=5;
+    $clothes['Shoes']+=20;
+    echo $clothes['Shoes'];
+
+    $contact = [
+        'Ricardo'=>[
+            'tel'=>'677777777',
+            'Email'=>'ricardo@gmail.com'
+        ],
+        'Michael'=>[
+            'tel'=>'606060606',
+            'Email'=>' mj@gmail.com'
+        ],
+        'Emmanuel'=>[
+            'tel'=>'610101010',
+            'Email'=>' manu@gmail.com'
+        ]
+    ];
+
+    echo $contact['Ricardo']['tel'] . '<br>';
+    echo $contact['Ricardo']['Email'];
+    var_dump ($contact);
+
+
     ?>
 </body>
 </html>
